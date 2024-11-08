123b038af2c2b4c4e59fde0c7b745ad71bae4f85
AHNayef <ahnayef@outlook.com>
2024-11-08 18:52:02 +0600

    Implements joi valodation

--- src/controllers/resgister.ts
+++ src/controllers/resgister.ts
@@ -1,25 +1,61 @@
 import { Request, Response } from "express";
+import Joi from "joi";
+import { connectToDatabase } from "../utils/db.util";
+
+
+const schema = Joi.object({
+    role: Joi.string().valid("Teacher", "Student", "CR").required(),
+    sid: Joi.string().pattern(/^056\d{13}$/).required().messages({
+        'string.pattern.base': 'ID must be 16 digits starting with 056',
+    }),
+    name: Joi.string().min(5).required(),
+    email: Joi.string().allow(null).optional()
+        .pattern(/^[a-zA-Z0-9._%+-]+@neub\.edu\.bd$/)
+        .messages({
+            'string.pattern.base': 'Email must be a valid neub.edu.bd address',
+        }),
+    password: Joi.string()
+        .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)
+        .required()
+        .messages({
+            'string.pattern.base': 'Password must be 8+ charecter with upper, lower, number, and special charecter',
+        }),
+    department: Joi.string().valid("CSE", "EEE", "BBA", "ENG").required(),
+    session: Joi.string().allow(null).optional()
+})
 
 const Register = async (req: Request, res: any) => {
     try {
-        console.log(req.body);
-        // const { role, sid, name, email, password, department, session } = req.body;
+        const db = await connectToDatabase();
+        const { error, value } = schema.validate(req.body);
 
-        // if (!role || !sid || !name || !email || !password || !department || !session) {
-        //     return res.status(400).json({
-        //         message: "All fields are required"
-        //     })
-        // }
+        if (error) {
+            return res.status(400).send(error.message);
+        }
+        const { role, sid, name, email, password, department, session } = value;
 
-        // if (role == "Teacher") {
-        //     // Check if teacher exists
-        //     // If teacher exists, return error
-        //     // If teacher does not exist, create teacher
-        // } else {
-        //     // Check if  exists
-        //     // If exists, return error
-        //     // If does not exist, create account
-        // }
+
+        if (role == "Teacher") {
+            // TODO:Check if teacher exists
+            // If teacher exists, return error
+            // If teacher does not exist, create teacher
+
+            const exist = await db.query("SELECT * FROM users WHERE email = ?", [email]);
+
+            if (exist) {
+                return res.status(400).send("User already exists");
+            }
+
+            
+
+            const user = await db.query("INSERT INTO users (role, name, email, password, department) VALUES (?, ?, ?, ?, ?, ?, ?)", [role, name, email, password, department]);
+            
+
+        } else {
+            //TODO: Check if exists
+            // If exists, return error
+            // If does not exist, create account
+        }
 
 
     } catch (error) {
