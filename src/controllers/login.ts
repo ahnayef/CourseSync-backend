0a10c8920752b4b42d7dcb1d6bc7a012d7d554f0
AHNayef <ahnayef@outlook.com>
2024-11-08 19:34:15 +0600

    Implemented basic login without JWT

--- src/controllers/login.ts
+++ src/controllers/login.ts
@@ -1,13 +1,30 @@
+import Joi from "joi";
 import { connectToDatabase } from "../utils/db.util";
+import bcrypt from 'bcrypt';
+
+
+const schema = Joi.object({
+    role: Joi.string().valid("Teacher", "Student", "CR").required(),
+    identification: Joi.string().required(),
+    password: Joi.string().required()
+})
 
 const Login = async (req: any, res: any) => {
     try {
-        const { role, identification, password } = req.body;
 
-        const connection = await connectToDatabase();
+        const db = await connectToDatabase();
+
+        const { error, value } = schema.validate(req.body);
+
+        if (error) {
+            return res.status(400).send(error.message);
+        }
+
+        const { role, identification, password } = value;
+
 
         if (role == 'Teacher') {
-            const [rows]: any = await connection.query(
+            const [rows]: any = await db.query(
                 `SELECT email FROM users WHERE email = ?`,
                 [identification]
             );
@@ -15,8 +32,21 @@ const Login = async (req: any, res: any) => {
             if (rows.length == 0) {
                 res.status(404).send('User not found');
             } else {
-                res.status(200).send('User found');
-                // TODO: Add the logic for the teacher login
+                const [user]: any = await db.query(
+                    `SELECT * FROM users WHERE email = ?`,
+                    [identification]
+                );
+
+                const passwordMatch = await bcrypt.compare(password, user[0].password);
+
+                if (!passwordMatch) {
+                    return res.status(401).send('Password does not match');
+                }
+
+                console.log(user[0]);
+                res.status(200).send(user[0]);
+
+
             }
         } // TODO: Add the logic for the student and CR login
     } catch (err) {
