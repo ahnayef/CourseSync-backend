09de45d5bc437b2d6ce23aabd9dfcbafde9252c3
AHNayef <ahnayef@outlook.com>
2024-11-09 23:55:18 +0600

    Setup auth middleware

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,1 +1,26 @@
-// TODO: Implement the auth middleware
+import { NextFunction, Request } from "express";
+import jwt from "jsonwebtoken";
+
+const authMiddleware = (req: Request, res: any, next: NextFunction) => {
+
+    const secretKey = process.env.JWT_SECRET_KEY;
+
+    if (!secretKey) {
+        return res.status(500).send('JWT secret key is not defined');
+    }
+
+    const token = req.header('Authorization');
+
+    if (!token) {
+        return res.status(401).send('Access denied. No token provided');
+    }
+
+    try {
+        const decoded = jwt.verify(token, secretKey);
+        // TODO: Complete the implementation 
+        next();
+    } catch (error) {
+        res.status(400).send('Invalid token');
+    }
+
+}   
