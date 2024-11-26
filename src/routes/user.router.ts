8436636203d8174c38e69725fb5c69d5a27e6463
AHNayef <ahnayef@outlook.com>
2024-11-26 12:21:59 +0600

    Add hodGet endpoint to retrieve students by department for HOD role

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import Login from "../controllers/user/login";
 import Register from "../controllers/user/resgister";
 import authMiddleware from "../middlewares/authMiddleware";
+import hodGet from "../controllers/user/hodGet";
 
 const UsersRouter = Router();
 
@@ -10,5 +11,7 @@ const UsersRouter = Router();
 UsersRouter.post('/login', Login)
 
 UsersRouter.post('/register', Register)
+
+UsersRouter.get('/hodGet', authMiddleware, hodGet)
 
 export { UsersRouter };
