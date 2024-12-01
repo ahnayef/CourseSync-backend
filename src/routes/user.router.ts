b89c099f980babda0446487ecc9fdc26d43f783c
AHNayef <ahnayef@outlook.com>
2024-12-01 17:21:41 +0600

    Add updateRole endpoint and modify hodGet to include 'cr' role

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -4,6 +4,7 @@ import Register from "../controllers/user/resgister";
 import authMiddleware from "../middlewares/authMiddleware";
 import hodGet from "../controllers/user/hodGet";
 import getOne from "../controllers/user/getOne";
+import updateRole from "../controllers/user/updateRole";
 
 const UsersRouter = Router();
 
@@ -16,5 +17,7 @@ UsersRouter.post('/register', Register)
 UsersRouter.get('/hodGet', authMiddleware, hodGet)
 
 UsersRouter.get('/getOne/:id', authMiddleware, getOne)
+
+UsersRouter.put('/updateRole', authMiddleware, updateRole)
 
 export { UsersRouter };
