b6854381e301fafc58e1653ba7df538771e8e065
AHNayef <ahnayef@outlook.com>
2025-01-15 00:19:23 +0600

    Allow 'cr' role to access schedule data alongside 'student' role

--- src/controllers/schedules/get.ts
+++ src/controllers/schedules/get.ts
@@ -33,7 +33,7 @@ const getSchedule = async (req: any, res: any) => {
                 data: rows
             });
 
-        } else if (user.role == "student") {
+        } else if (user.role == "student" || user.role == "cr") {
 
             const [rows]: any = await db.query("SELECT s.id, s.day, s.department, s.session, s.start, s.end, s.room, courses.name AS name, courses.code AS courseCode,  users.name AS teacher FROM schedules AS s INNER JOIN courses ON courses.id = s.course INNER JOIN users ON users.id = courses.instructor  INNER JOIN enroll ON enroll.course_id = s.course WHERE enroll.student_id = ? ORDER BY s.start", [user.id]);
 
